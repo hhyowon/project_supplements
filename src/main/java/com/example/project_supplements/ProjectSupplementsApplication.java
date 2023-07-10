e9a798a342faeb1982ec8fd8c7df9593f6867eff
hhyowon <hyowon0605@gmail.com>
2023-07-10 15:31:49 +0900

    first commit

---
+++ src/main/java/com/example/project_supplements/ProjectSupplementsApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_supplements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSupplementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSupplementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_supplements/ProjectSupplementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_supplements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSupplementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
