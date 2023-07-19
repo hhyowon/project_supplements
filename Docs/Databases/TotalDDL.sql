ece2e5c910df28270c0db3e0f84d1fb5f754a059
hhyowon <hyowon0605@gmail.com>
2023-07-19 18:08:59 +0900

    효욧

--- Docs/Databases/TotalDDL.sql
+++ Docs/Databases/TotalDDL.sql
@@ -38,7 +38,7 @@ CREATE TABLE COMMONS_CODE
   ATTRIBUTION_1         varchar(60)   NULL     COMMENT '속성_1',
   ATTRIBUTION_2         varchar(60)   NULL     COMMENT '속성_2',
   DESCRIPTION           varchar(1000) NULL     COMMENT '코드설명',
-  SYSTEM_CODE_YN        varchar(40)   NOT NULL DEFAULT N COMMENT '초기 시스템 코드 여부',
+  SYSTEM_CODE_YN        varchar(40)   NOT NULL COMMENT '초기 시스템 코드 여부',
   USE_YN                varchar(40)   NOT NULL COMMENT '사용 여부',
   PARENT_COMMON_CODE_ID varchar(40)   NULL     COMMENT '상위 공통 코드 ID',
   REGISTER_SEQ          varchar(50)   NOT NULL COMMENT '등록자SEQ',
--- src/main/java/com/example/project_supplements/service/SearchService.java
+++ src/main/java/com/example/project_supplements/service/SearchService.java
@@ -1,5 +1,20 @@
 package com.example.project_supplements.service;
+import java.util.HashMap;
+import java.util.Map;
 
-public class SearchService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.project_supplements.dao.SharedDao;
+import com.example.project_supplements.utils.Paginations;
+
+
+@Service
+@Transactional
+public class SearchService  {
+    @Autowired
+    SharedDao sharedDao;
+
     
 }
