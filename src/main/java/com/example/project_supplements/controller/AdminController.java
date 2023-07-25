4a0e565382f1429b8f094af4c9523b1a8f2bd110
AKAVespaET3 <quattrini244@gmail.com>
2023-07-25 18:52:40 +0900

    goni

--- src/main/java/com/example/project_supplements/controller/AdminController.java
+++ src/main/java/com/example/project_supplements/controller/AdminController.java
@@ -13,13 +13,13 @@ import com.example.project_supplements.service.AdminService;
 
 @Controller
 
-@RequestMapping("/admin")
+@RequestMapping("/admina")
 
 public class AdminController {
     @Autowired
     AdminService adminService;
 
-    @GetMapping({""})
+    @GetMapping({"/"})
     public ModelAndView main(@RequestParam Map<String, String> params, ModelAndView modelAndView) {
         Object result = adminService.selectWithPagination(params); // params 맵을 인자로 전달하여 검색 결과를 가져 옴
 
