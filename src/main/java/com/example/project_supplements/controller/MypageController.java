4bff60396f1538f615c8fee9b26c955c2dc7f455
GabaeOh <rkqo4577@gmail.com>
2023-07-27 18:30:18 +0900

    rkqo

--- src/main/java/com/example/project_supplements/controller/MypageController.java
+++ src/main/java/com/example/project_supplements/controller/MypageController.java
@@ -101,17 +101,18 @@ public class MypageController {
     //     modelAndView.setViewName("/WEB-INF/views/mypage/community.jsp");
     //     return modelAndView;
     // }
+   
    //커뮤니티 게시글 수정
-    @GetMapping("/communityupdateAndSelectSearch")
-    public ModelAndView communityupdateAndSelectSearch( @RequestParam Map params,  ModelAndView modelAndView) {
-        Object result = mypageService.communityupdateAndSelectSearch(params);
+   @GetMapping("/communityupdateAndSelectSearch/{COMMUNITY_ID}")
+   public ModelAndView communityupdateAndSelectSearch(@PathVariable String COMMUNITY_ID, @RequestParam Map params,  ModelAndView modelAndView) {
+       Object result = mypageService.communityupdateAndSelectSearch(COMMUNITY_ID, params);
 
-        modelAndView.addObject("params", params);
-        modelAndView.addObject("result", result);
-        modelAndView.setViewName("/WEB-INF/views/mypage/mypage_community.jsp");
+       modelAndView.addObject("params", params);
+       modelAndView.addObject("result", result);
+       modelAndView.setViewName("/WEB-INF/views/mypage/mypage_community.jsp");
 
-        return modelAndView;
-    }
+       return modelAndView;
+   }
     
     @GetMapping("/BMI")
     public ModelAndView selectSearch(@RequestParam Map params,ModelAndView modelAndView) {
