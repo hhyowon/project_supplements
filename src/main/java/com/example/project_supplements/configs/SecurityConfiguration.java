8de7e975012f38bdcfecc160fbe0c3619238ae74
YebinKim98 <dpqls9898@gmail.com>
2023-07-31 15:52:45 +0900

    goni

--- src/main/java/com/example/project_supplements/configs/SecurityConfiguration.java
+++ src/main/java/com/example/project_supplements/configs/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration {
                 // url, roles from Dao
                 httpSecurity.authorizeHttpRequests() // 로그인
                 .requestMatchers("/manager*").hasAnyRole("ADMIN", "MANAGER")
-                .requestMatchers("/admin*").hasRole("ADMIN")
+                .requestMatchers("/admin*/*").hasRole("ADMIN")
                 .requestMatchers("/main").authenticated() //로그인한 대상자
                 .requestMatchers("/carInfor/map/*").hasRole("USER")
                         .anyRequest().permitAll() //그외 전체 대상
