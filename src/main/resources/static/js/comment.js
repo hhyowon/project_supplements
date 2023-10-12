67471bd153ca850293802c7f376b780ea1e4bae4
AKAVespaET3 <quattrini244@gmail.com>
2023-10-13 04:41:27 +0900

    goni

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -21,45 +21,47 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             const resultList = data.commentresult.resultList;
             // 'commentresult' 안에 'resultList'를 찾습니다.
+        
+            // 컨테이너 초기화 (기존 댓글 삭제)
+            const container = document.getElementById("commentTableContainer");
+            container.innerHTML = '';
+        
             resultList.forEach(item => {
                 const COMMENT_USER_ID = item.COMMENT_USER_ID;
                 const COMMENT = item.COMMENT;
                 const COMMENTDATE = item.COMMENTDATE;
-
-                // 여기서 데이터를 사용하거나 처리합니다.
-            });
-            let newComment = document.createElement("table");
-            newComment.className = "tb tb_row";
-            newComment.style.border = "1px solid #ececec";
-            newComment.style.width = "100%";
-
-            let content = `
-            <colgroup>
-                    <col style="width: 20%;" />
-                    <col style="width: 60%;" />
-                    <col style="width: 20%;" />
-                </colgroup>
-                <tbody>
-                    <tr style="border: 1px solid #ececec;">
-                        <th scope="row" style="background-color: #eeeeee; text-align: center; width: 20%;">
-                            ${COMMENT_USER_ID}
-                        </th>
-                        <td style="width: 60%;">
-                            ${COMMENT}
-                        </td>
-                        <td style="width: 20%;">
-                            ${COMMENTDATE}
-                        </td>
-                    </tr>
-                </tbody>
+        
+                let newComment = document.createElement("table");
+                newComment.className = "tb tb_row";
+                newComment.style.border = "1px solid #ececec";
+                newComment.style.width = "100%";
+        
+                let content = `
+                <colgroup>
+                        <col style="width: 20%;" />
+                        <col style="width: 60%;" />
+                        <col style="width: 20%;" />
+                    </colgroup>
+                    <tbody>
+                        <tr style="border: 1px solid #ececec;">
+                            <th scope="row" style="background-color: #eeeeee; text-align: center; width: 20%;">
+                               ${COMMENT_USER_ID}
+                            </th>
+                            <td style="width: 60%;">
+                               ${COMMENT}
+                            </td>
+                            <td style="width: 20%;">
+                               ${COMMENTDATE}
+                            </td>
+                        </tr>
+                    </tbody>
                 `;
-
-            newComment.innerHTML = content;
-            document.getElementById("commentTableContainer").appendChild(newComment);
+        
+                newComment.innerHTML = content;
+                container.appendChild(newComment);
+            });
+            
             commentInput.value = '';
-        })
-        .catch(error => {
-            console.error("Error:", error);
         });
     });
 });
